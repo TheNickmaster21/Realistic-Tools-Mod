ef45da75d883b18b41ee9fe0341b779fc2859ccb
Nick Main <nickmaster24@gmail.com>
2016-03-12 16:56:40 -0500

    Initial Commit

---
+++ src/main/java/in/nickma/realistictools/NickMod.java
@@ -0,0 +1,20 @@
+package in.nickma.realistictools;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+
+@Mod(modid = NickMod.MODID, version = NickMod.VERSION)
+public class NickMod {
+
+    public static final String MODID = "RealisticTools";
+    public static final String VERSION = "0.1";
+    public static final String MOD_PREFIX = MODID + ":";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+}
